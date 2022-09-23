2409f2bf0e5fe19a2a49ac69a9e4a9d6bb00f46e
lauramagallanes <pasocenturiontours@gmail.com>
2022-09-23 18:39:46 +0000

    slicing de la cadena del noun

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
 var ext = [".com", ".net", ".us", ".io"];
 
+// for (let i = 0; i < pronoun.length; i++) {
+//     for (let j = 0; j < adj.length; j++) {
+//         for (let k = 0; k < noun.length; k++) {
+//             for (let n = 0; n < ext.length; n++) {
+//                 console.log(pronoun[i] + adj[j] + noun[k] + ext[n]);
+//             }
+//         }
+//     }
+// }
+
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
             for (let n = 0; n < ext.length; n++) {
-                console.log(pronoun[i] + adj[j] + noun[k] + ext[n]);
+                console.log(
+                    pronoun[i] +
+                    adj[j] +
+                    noun[k].slice(0, 4) +
+                    "." +
+                    noun[k].slice(4, 6) +
+                    ext[n]
+                );
             }
         }
     }
 }
 
 //thebigraco.on.io
+// jogger
+// 012345
+//slice(0,4)
